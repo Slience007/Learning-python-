fa5123bf3e54a87e729aaf38ff651a5249585702
Stephen <1280353877@qq.com>
2018-12-21 22:54:50 +0800

    add_nums

--- funcTest.py
+++ funcTest.py
@@ -2,6 +2,16 @@
 def add_func(a,b):
     c =a + b
     print(c)
-
 if __name__ =="__main__":
     add_func(2,3)
+ #参数可变化
+def add_nums(x,*rgs):
+    print("First arg is: %s" %x)
+    result = x
+    print(rgs)
+    for i in rgs:
+        result = result + i
+    print("Result is :%s" %result)
+
+add_nums(0,1,3,5)
+
